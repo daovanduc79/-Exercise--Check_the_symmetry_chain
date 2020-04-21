7ee6b511939e2d49fa5fae002fc55980d55d4ee5
daovanduc <duc07092000@gmail.com>
2020-04-22 02:55:28 +0700

    update index

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include_once 'class/QueueChain.php';
+include_once 'class/StackChain.php';
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Check the symmetry chain</title>
+</head>
+<body>
+<form method="post" action="action/action.php">
+    <label>String : <input type="text" name="string"></label>
+    <input type="submit" value="check">
+</form>
+</body>
+</html>
